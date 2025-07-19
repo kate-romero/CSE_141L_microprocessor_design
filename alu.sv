4b4e29ba39cfe10654c3d08a7e0ff25942f2bb27
kate-romero <kgromero@ucsd.edu>
2025-07-18 22:09:36 -0700

    completed reg_file, tidied up alu

--- alu.sv
+++ alu.sv
@@ -1,38 +1,44 @@
 // combinational -- no clock
-// sample -- change as desired
+// debug starting place: need 'assign?'
+/* Questions for TA:
+ * load, store, move, compare in ALU or elsewhere?
+ * need pari, zero outputs?
+ */
 module alu(
   input[3:0] alu_cmd,    // ALU instruction
   input[7:0] inA, inB,	 // 8-bit wide data path; inA is always implied register
   input      sc_i,       // shift_carry in
   output logic[7:0] rslt,// almost always sent to implied register
   output logic sc_o,     // shift_carry out
-               pari,     // reduction XOR (output); TODO: not needed?
-			   zero      // NOR (output); TODO: not needed?
+               pari,     // reduction XOR (output)
+			   zero      // NOR (output)
 );
 
 always_comb begin 
   rslt = 'b0;            
   sc_o = 'b0;    
-  zero = !rslt; // needed?
-  pari = ^rslt; // needed?
+  zero = !rslt;
+  pari = ^rslt;
   case(alu_cmd)
 
-	4'b0000:	// TODO: in ALU? load from memory
-	4'b0001:	// TODO: in ALU? store to memory
-	4'b0010:	// in ALU? move from reg inB to inA
+	4'b0000:	// load from memory
 	  rslt = inB;
-	4'b0011:	// in ALU? move to reg inB from inA
+	4'b0001:	// store to memory
 	  rslt = inA;
-	4'b0100:	// in ALU? move immediate to register; immediate is inB?
+	4'b0010:	// move from reg inB to inA
 	  rslt = inB;
-	4'b0101:	// in ALU? compare
+	4'b0011:	// move to reg inB from inA
+	  rslt = inA;
+	4'b0100:	// move immediate to register; immediate is inB
+	  rslt = inB;
+	4'b0101:	// compare
 	  if (inA == inB)
 	    rslt = 8'b00000000;
 	  else if (inA < inB)
 	    rslt = 8'b00000001;
 	  else
 	    rslt = 8'b00000010;
-	4'b0110:	// bitwise NOT; flips inB?
+	4'b0110:	// bitwise NOT; flips inB
 	  rslt = ~inB;
 	4'b0111:	// bitwise OR
 	  rslt = inA | inB;
